94f089ac98ca341be558b4d2b31292e8aeaac32c
Angel Rose <angelrose.bernados@gmail.com>
2021-02-23 03:18:58 +0800

    sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,32 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/views/**/*.blade.php'
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans]
             },
-        },
+            colors: {
+                cyan: colors.cyan,
+                blue: colors.blue,
+                primary: '#0D47A1'
+            }
+        }
     },
 
     variants: {
         extend: {
-            opacity: ['disabled'],
-        },
+            opacity: ['disabled']
+        }
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
 };
