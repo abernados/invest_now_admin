ade2b3cbe9a843b673edba97860c667de93a12ba
Pamela Villarama <villaramapamela@gmail.com>
2021-03-03 01:50:50 +0800

    fix custom colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,20 +22,20 @@ module.exports = {
                 orange: colors.orange,
                 red: colors.red,
                 blue: colors.blue,
-                primary: '#0D47A1'
+                primary: '#0D47A1',
+                custom: {
+                    gradient_blue_dark: '#0A367B',
+                    gradient_blue_light: '#0D47A1',
+                    gradient_violet_dark: '#6C63FF',
+                    gradient_violet_medium: '#7B72FF',
+                    gradient_violet_light: '#837BFF'
+                }
             },
             spacing: {
                 '108': '27rem'
             },
             primary: '#0D47A1',
             sidebarActive: '#FFF0A7',
-            custom: {
-                gradient_blue_dark: '#0A367B',
-                gradient_blue_light: '#0D47A1',
-                gradient_violet_dark: '#6C63FF',
-                gradient_violet_medium: '#7B72FF',
-                gradient_violet_light: '#837BFF'
-            }
         }
     },
 
