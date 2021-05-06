bb8d6b78eca8c968f3d4766942c0b4d6fc44f373
Angel Rose <angelrose.bernados@gmail.com>
2021-05-07 00:53:48 +0800

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,54 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11">
+                <!-- Social login form-->
+                <div class="card my-5">
+                    <div class="card-body p-5 text-center">
+                        <div class="h3 font-weight-light">Sign In</div>
+                    </div>
+                    <hr class="my-0" />
+                    @if (session('status'))
+                        <div class="mb-4 font-medium text-sm text-green-600">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <div class="card-body p-5">
+                        <!-- Login form-->
+                        <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <!-- Form Group (email address)-->
+                            <div class="form-group">
+                                <label class="text-gray-600 small" for="emailExample">Email address</label>
+                                <input class="form-control form-control-solid" type="text" placeholder aria-label="Email Address" aria-describedby="emailExample" name="email" :value="old('email')" required autofocus />
+                            </div>
+                            <!-- Form Group (password)-->
+                            <div class="form-group">
+                                <label class="text-gray-600 small" for="passwordExample">Password</label>
+                                <input class="form-control form-control-solid" type="password" placeholder aria-label="Password" aria-describedby="passwordExample" name="password" required autocomplete="current-password" />
+                            </div>
+                            <!-- Form Group (forgot password link)-->
+                            <div class="form-group"><a class="small" href="auth-password-social.html">Forgot your password?</a></div>
+                            <!-- Form Group (login box)-->
+                            <div class="form-group d-flex align-items-center justify-content-between mb-0">
+                                <div class="custom-control custom-control-solid custom-checkbox">
+                                    <input class="custom-control-input small" id="customCheck1" type="checkbox" />
+                                    <label class="custom-control-label" for="customCheck1">Remember password</label>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                    <hr class="my-0" />
+                    <div class="card-body px-5 py-4">
+                        <div class="small text-center">
+                            New user?
+                            <a href="auth-register-social.html">Create an account!</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
+        </div>
+    </div>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
 </x-guest-layout>
