dcdfc493ee730fc4ba5eeeff36509278b3bcbf88
Angel Rose <angelrose.bernados@gmail.com>
2021-05-07 01:19:40 +0800

    user avatar with logout btn

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -38,6 +38,29 @@
                 </div>
             </div>
         </form>
+        <ul class="navbar-nav align-items-center ml-auto">
+            <li class="nav-item dropdown no-caret mr-2 dropdown-user">
+                <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" /></a>
+                <div class="dropdown-menu dropdown-menu-right border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownUserImage">
+                    <h6 class="dropdown-header d-flex align-items-center">
+                        <img class="dropdown-user-img" src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" />
+                        <div class="dropdown-user-details">
+                            <div class="dropdown-user-details-name">Valerie Luna</div>
+                            <div class="dropdown-user-details-email">vluna@aol.com</div>
+                        </div>
+                    </h6>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#!">
+                        <div class="dropdown-item-icon"><i data-feather="settings"></i></div>
+                        Account
+                    </a>
+                    <a class="dropdown-item" href="#!">
+                        <div class="dropdown-item-icon"><i data-feather="log-out"></i></div>
+                        Logout
+                    </a>
+                </div>
+            </li>
+        </ul>
     </nav>
     <div id="layoutSidenav">
         @include('layouts.sidebar')
