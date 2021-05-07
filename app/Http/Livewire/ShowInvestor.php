7836db1c54f5f750333bd32821ff4b322fe04192
Airhon Carlo Cortez <acgcortez@multisyscorp.com>
2021-05-07 23:08:23 +0800

    fixed bug

--- app/Http/Livewire/ShowInvestor.php
+++ app/Http/Livewire/ShowInvestor.php
@@ -32,27 +32,27 @@ class ShowInvestor extends Component
 
         $this->investor_card_details = $this->investor->cardDetails;
 
-        $this->first_name = $this->investor->first_name;
-        $this->last_name = $this->investor->last_name;
-        $this->email_address = $this->investor->email;
+        $this->first_name = $this->ifNull($this->investor->first_name);
+        $this->last_name = $this->ifNull($this->investor->last_name);
+        $this->email_address = $this->ifNull($this->investor->email);
 
-        $this->phone_number = $this->investor_address_info->phone_number;
-        $this->country = $this->investor_address_info->country;
-        $this->address = $this->investor_address_info->address;
-        $this->city = $this->investor_address_info->city;
-        $this->state = $this->investor_address_info->state;
-        $this->zip = $this->investor_address_info->zip;
+        $this->phone_number = $this->ifNull($this->investor_address_info->phone_number);
+        $this->country = $this->ifNull($this->investor_address_info->country);
+        $this->address = $this->ifNull($this->investor_address_info->address);
+        $this->city = $this->ifNull($this->investor_address_info->city);
+        $this->state = $this->ifNull($this->investor_address_info->state);
+        $this->zip = $this->ifNull($this->investor_address_info->zip);
 
-        $this->social_security_number = $this->investor_requirements->social_security_number;
-        $this->date_of_birth = Carbon::parse($this->investor_requirements->date_of_birth)->format('M d, Y');
-        $this->investment_amount =  '$' . number_format($this->investor_investment_values->amount, 2, '.', ',');
+        $this->social_security_number = $this->ifNull($this->investor_requirements->social_security_number);
+        $this->date_of_birth = isset($this->investor_requirements->date_of_birth) ? Carbon::parse($this->investor_requirements->date_of_birth)->format('M d, Y') : null;
+        $this->investment_amount =  '$' . number_format($this->ifNull($this->investor_investment_values->amount), 2, '.', ',');
 
         $this->subscription_agreement = isset($this->investor_documents->subscription_agreement) ? $this->investor_documents->subscription_agreement : null; 
         $this->sample_broker_dealer_aggreement = isset($this->investor_documents->sample_broker_dealer_aggreement) ? $this->investor_documents->sample_broker_dealer_aggreement : null; 
         $this->another_agreement = isset($this->investor_documents->another_agreement) ? $this->investor_documents->another_agreement : null; 
 
-        $this->card_expiry = $this->investor_card_details->expiration_month . '/' . $this->investor_card_details->expiration_year;
-        $this->card_last_four_digits = $this->investor_card_details->last_four_digits;
+        $this->card_expiry = $this->ifNull($this->investor_card_details->expiration_month) . '/' . $this->ifNull($this->investor_card_details->expiration_year);
+        $this->card_last_four_digits = $this->ifNull($this->investor_card_details->last_four_digits);
     }
 
     public function getInvestor($investor_id)
@@ -65,20 +65,25 @@ class ShowInvestor extends Component
         if ($investor->accreditedInvestors) {
             $this->accredited_investor = 'Not Accredited';
             
-            $this->annual_income = '$' . number_format($investor->accreditedInvestors->annual_income, 2, '.', ',');
-            $this->net_worth = '$' . number_format($investor->accreditedInvestors->net_worth, 2, '.', ',');
+            $this->annual_income = '$' . number_format( isset($investor->accreditedInvestors->annual_income) ? $investor->accreditedInvestors->annual_income : 0 , 2, '.', ',');
+            $this->net_worth = '$' . number_format(isset($investor->accreditedInvestors->annual_income) ? $investor->accreditedInvestors->annual_income : 0, 2, '.', ',');
 
             return;
         }
 
         $this->accredited_investor = 'Accredited';
 
-        $this->annual_income = '$' . number_format($investor->additionalnfos->annual_income, 2, '.', ',');
-        $this->net_worth = '$' . number_format($investor->additionalnfos->net_worth, 2, '.', ',');
+        $this->annual_income = '$' . number_format(isset($investor->additionalnfos->annual_income) ? $investor->additionalnfos->annual_income :0 , 2, '.', ',');
+        $this->net_worth = '$' . number_format(isset($investor->additionalnfos->annual_income) ? $investor->additionalnfos->annual_income : 0, 2, '.', ',');
     }
 
     public function render()
     {
         return view('investors.show')->layout('layouts.default', ['simple' => true]);
+    }
+
+    private function ifNull($detail)
+    {
+        return isset($detail) ? $detail : null;
     }
 }
